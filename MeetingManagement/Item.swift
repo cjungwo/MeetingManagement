34c5eba89ac5e2ce50e182faf72f5585ffd5823b
Cjungwo <jwoo516@naver.com>
2024-10-10 15:22:18 +1100

    Initial Commit

---
+++ MeetingManagement/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MeetingManagement
+//
+//  Created by JungWoo Choi on 10/10/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
